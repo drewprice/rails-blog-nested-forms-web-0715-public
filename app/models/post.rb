8b8cb6a7f78fbe911125eee28b297b5ec1dc0477
Drew Price <drewprice@users.noreply.github.com>
2015-09-14 15:36:31 -0400

    Add solution

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,16 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :post_tags
-  has_many :tags, :through => :post_tags
+  has_many :tags, through: :post_tags
 
   validates_presence_of :name, :content
+
+  def new_tag=(tag_params)
+    if tag_params[:name]
+      persisted = Tag.find_by(tag_params)
+      tag = persisted ? persisted : tags.build(tag_params)
+      return tag
+    end
+
+  end
 end
